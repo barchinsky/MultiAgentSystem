3f05c3e89543f4a16003800435255c1dd8c3a38e
barchinsky <barchinsky@gmail.com>
2013-10-30 01:24:48 +0200

    Fix bugs connected with continuing moving even if server connection has not been setuped.

--- Ant.py
+++ Ant.py
@@ -10,10 +10,10 @@ import math
 import random
 
 sys.path.append('templates')
-from json_templates import *
+from templates import *
 
 class Ant:
-    def __init__(self):
+    def __init__(self,exteranl_id):
         self._config=ConfigParser.RawConfigParser()
         self._config.read('conf/config.conf')
         self._host=self._config.get("Ant","host")
@@ -21,14 +21,20 @@ class Ant:
         
         self.client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM) # socket connects to anthell server
 
+        self._external_id = exteranl_id
         self._id=int(time.time())
-        self._pos_x=None
-        self._pos_y=None
+        self._pos_x=0
+        self._pos_y=0
 
         self._passed_way_to_resource=[] #array of passed way (x,y) coordinate to resource dislocation
 
     def connect(self,host,port):
-        self.client_socket.connect((host,port)) # connect to host
+        try:
+            self.client_socket.connect((host,port)) # connect to host
+        except Exception,e:
+            print self._external_id
+            print e
+            sys.exit()
 
     def disconnect(self):
         self.client_socket.close()
@@ -37,7 +43,7 @@ class Ant:
         '''
         Provide client registration
         '''
-        reg_query = self.get_reg_query(self._id)
+        reg_query = self.create_reg_query(self._id)
         print "register::Registration query:",reg_query
 
         self.send(reg_query)
@@ -46,13 +52,13 @@ class Ant:
         '''
         Send data to server
         '''
-        print "send::Query to send:",query
+        print "%s:send::Query to send:%s"%(self._external_id,query)
 
         try:
             self.client_socket.sendall(query.encode('utf-8'))
-            print "send::Data sended."
+            print "%s:send::Data sended."%self._external_id
             response = self.client_socket.recv(1024)
-            print "send::Response goted."
+            print "%s:send::Response goted."%self._external_id
 
             if response:
                 self.process_response(response)
@@ -78,25 +84,32 @@ class Ant:
 
         elif api_key == "is_ant_can_move":
             print "process_response::Is ant can move method called."
-            warnings.warn("process_response::Need to define this part of process_response() method.",Warning)
             x,y = j["OBJECT"]["COORD_ANT"]
             print "process_response::New coordinates goted:",x,y
+            self._pos_x=x
+            self._pos_y=y
             self._passed_way_to_resource.append((x,y))
         elif api_key == "ERROR":
             print "process_response::Error section found."
+        elif api_key == "nearest_object":
+            pass
 
-    def get_reg_query(self,ant_id):
+    def create_reg_query(self,ant_id):
         '''
         Create registration query.
         '''
         query={"API_KEY":"registration","OBJECT":{}}
         return json.dumps(query)
 
-    def get_is_ant_can_move_query(self,vector_x,vector_y):
+    def create_is_ant_can_move_query(self,vector_x,vector_y):
         '''
         Generate json is_ant_can_move query.
         '''
         query={"API_KEY":"is_ant_can_move","OBJECT":{"VECTOR":[vector_x,vector_y]} }
+        return json.dumps(query)
+
+    def create_get_nearest_object_query(self):
+        query={"API_KEY":"nearest_objects","OBJECT":{"COORD":[self._pos_x,self._pos_y],"VISION_RANGE":0.05}}
         return json.dumps(query)
 
     def get_possible_direction(self):
@@ -105,26 +118,15 @@ class Ant:
         posible_way - array of possible moving directions
         '''
         rad = random.randint(0,360)
-        vector_x = 1.#math.cos(rad)
-        vector_y = 0.#math.sin(rad)
-        self.send(self.get_is_ant_can_move_query(vector_x, vector_y))
-
-    def choose_way(self):
-        '''
-        Choose moving direction if there are several choices and move ant
-        '''
-        self.move(choosen_x,choosen_y)
-        pass
+        vector_x = math.cos(rad/K)
+        vector_y = math.sin(rad/K)
+        self.send(self.create_is_ant_can_move_query(vector_x, vector_y))
 
     def move(self):
         '''
         Move ant, should send data to server about choosen way
         '''
         self.get_possible_direction()
-
-        
-        #self._passed_way.append((new_x,new_y))
-        pass
 
     def send_passed_way(self):
         '''
@@ -149,14 +151,12 @@ class Ant:
         self.register()
         while True:
             self.move()
-
             print "live::I'm alive!"
-            time.sleep(1)
+            time.sleep(0.1)
 
 
 # /////////////////////// DEBUG ZONE ///////////////////////////
 if __name__=='__main__':
-    test_ant = Ant()
+    test_ant = Ant(0)
     
-    print 'test'
     test_ant.live()
